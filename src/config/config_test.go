2b4018e006e8af61b2453c7ba9588b30a50714d8
davidhilman <davhil212@gmail.com>
2022-12-10 11:11:27 +0000

    Remove commented tests

--- src/config/config_test.go
+++ src/config/config_test.go
@@ -116,31 +116,4 @@ EDIT_WAIT_SECONDS=10`,
 			require.Equal(t, test.want, cfg)
 		})
 	}
-
-	//t.Run("all values empty in file", func(t *testing.T) {
-	//
-	//})
-	//
-	//t.Run("should load all values from file", func(t *testing.T) {
-	//	cfg, err := LoadEnvConfig(fileName)
-	//	require.NoError(t, err)
-	//	require.Equal(t, []int64{123}, cfg.TelegramID)
-	//	require.Equal(t, "abc", cfg.TelegramToken)
-	//	require.Equal(t, 10, cfg.EditWaitSeconds)
-	//})
-	//
-	//t.Run("env variables should override file values", func(t *testing.T) {
-	//	unset := setEnvVariables(map[string]string{
-	//		"TELEGRAM_ID":       "456,789",
-	//		"TELEGRAM_TOKEN":    "def",
-	//		"EDIT_WAIT_SECONDS": "20",
-	//	})
-	//
-	//	cfg, err := LoadEnvConfig(fileName)
-	//	unset()
-	//	require.NoError(t, err)
-	//	require.Equal(t, []int64{456, 789}, cfg.TelegramID)
-	//	require.Equal(t, "def", cfg.TelegramToken)
-	//	require.Equal(t, 20, cfg.EditWaitSeconds)
-	//})
 }
